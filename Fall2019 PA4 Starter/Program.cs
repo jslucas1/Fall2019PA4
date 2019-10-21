21d207d27d3dcd2c33e6d9a035b2085f4f6f6117
Jeff Lucas <jslucas1@msn.com>
2019-10-21 07:53:02 -0500

    Add project files.

---
+++ Fall2019 PA4 Starter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2019_PA4_Starter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PA4 Starter Code
+        }
+    }
+}
